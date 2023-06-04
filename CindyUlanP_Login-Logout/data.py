f26ba2ef3f6e6ff7ef7fe0004d84ef294a8b79d7
cindyulan <cindyulanp03@gmail.com>
2023-06-04 20:56:15 +0700

    Update data.py

--- CindyUlanP_Login-Logout/data.py
+++ CindyUlanP_Login-Logout/data.py
@@ -1,4 +1,5 @@
 class input():
+    #Login & Logout
     invalidCred = "Invalid credentials"
     required = "Required"
     username = "Admin"
@@ -8,3 +9,43 @@ class input():
     invalidPassword = "abc123"
     loginTxt = "Login"
     dashboardTxt ="Dashboard"
+
+    #Add User
+    passMana = "Cobacoba123@"
+    confirmPass = "Cobacoba123@"
+    userManage = "cindy.coba"
+    employee = "Paul Collings"
+    reqMana = "Required"
+
+    #Useraname already exist
+    existUser = "Already exists"
+
+    #Blank field select
+    usnmanage = "blank.select"
+
+    #Blank employee name
+    usnmanage_1 = "employee.name"
+
+    #Blank password
+    usnmanage_2 = "blank.password"
+
+    #Invalid password
+    invPass = "lalapo"
+    passInv = "Should have at least 7 characters"
+
+    #Invalid confirm password
+    confirmInv = "Passwords do not match"
+
+    #Invalid username
+    invUser = "lala"
+    userInv = "Should be at least 5 characters"
+
+    #Search user
+    invName = "cindy coba"
+    foundName = "(1) Record Found"
+    foundRole = "(13) Records Found"
+    noRecord = "No Records Found"
+    reset = "-- Select --"
+
+    #Edit user
+    valEdit ="User Management"
